22565536a6b43153acefd61c185b9d1b9aacceeb
AniketGapat <aniketgapat0303@gmail.com>
2025-02-02 00:45:00 +0530

    secrets updated

--- src/test/java/Automation/Naukri/NaukriAutomation.java
+++ src/test/java/Automation/Naukri/NaukriAutomation.java
@@ -18,15 +18,16 @@ public class NaukriAutomation {
 
 	public static void main(String[] args) {
 		// Set up WebDriver
-	        WebDriverManager.chromedriver().clearDriverCache().setup();
-	        ChromeOptions options = new ChromeOptions();
-	        options.addArguments("--headless"); // Run browser in background
-	        options.addArguments("--window-size=1920,1080"); //Sets the browser window size to 1920x1080 pixels
-	        options.addArguments("--disable-gpu"); //Disables GPU hardware acceleration.
-	        options.addArguments("--no-sandbox"); //Disables the Chrome security sandbox
-	        options.addArguments("--disable-dev-shm-usage"); //Prevents Chrome from using /dev/shm (shared memory) for temporary storage
-	        options.addArguments("--enable-javascript"); //Ensures JavaScript execution is enabled in Chrome
-	        WebDriver driver = new ChromeDriver(options);
+		WebDriverManager.chromedriver().clearDriverCache().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless"); // Run browser in background
+		options.addArguments("--window-size=1920,1080"); // Sets the browser window size to 1920x1080 pixels
+		options.addArguments("--disable-gpu"); // Disables GPU hardware acceleration.
+		options.addArguments("--no-sandbox"); // Disables the Chrome security sandbox
+		options.addArguments("--disable-dev-shm-usage"); // Prevents Chrome from using /dev/shm (shared memory) for
+															// temporary storage
+		options.addArguments("--enable-javascript"); // Ensures JavaScript execution is enabled in Chrome
+		WebDriver driver = new ChromeDriver(options);
 
 		try {
 			// Maximize browser window
@@ -41,29 +42,26 @@ public class NaukriAutomation {
 			WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit'][1]"));
 
 			// Enter login credentials
-//	            emailField.sendKeys(System.getenv("NAUKRI_EMAIL")); // Email from environment variable
-//	            passwordField.sendKeys(System.getenv("NAUKRI_PASSWORD")); // Password from environment variable
-
-			emailField.sendKeys("aniketgapat0303@gmail.com");
-			passwordField.sendKeys("Aniket@333");
+			emailField.sendKeys(System.getenv("NAUKRI_USERNAME")); // Email from environment variable
+			passwordField.sendKeys(System.getenv("NAUKRI_PASSWORD")); // Password from environment variable
 
 			// Click login button
 			loginButton.click();
 			System.out.println("Profile SignIn");
-			
+
 			// Wait until the profile Button loads and click on that
 			WebElement viewButton = wait
 					.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='View profile']")));
 			viewButton.click();
-			
+
 			// Javascript executor to mimic human behaviour
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            js.executeScript("window.scrollBy(0,500)");
-            Thread.sleep(500);
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("window.scrollBy(0,500)");
+			Thread.sleep(500);
 			WebElement headline = wait.until(ExpectedConditions
 					.elementToBeClickable(By.xpath("//div[@class='card mt15']/div/div/span[@class='edit icon']")));
 			headline.click();
-			
+
 			// Get text from resume headline
 			WebElement data = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("resumeHeadlineTxt")));
 			String text = data.getText();
@@ -72,36 +70,37 @@ public class NaukriAutomation {
 			if (text.endsWith(".")) {
 				text = text.substring(0, text.length() - 1); // Remove the last dot
 			} else {
-				 text = text + "."; // Add a dot at the end
+				text = text + "."; // Add a dot at the end
 			}
-			
-			//clear the existing text and add updated text with or without dot
+
+			// clear the existing text and add updated text with or without dot
 			data.clear();
 			Thread.sleep(2000);
 			data.sendKeys(text);
-			
-			//Update profile by clicking on a button or filling a field
+
+			// Update profile by clicking on a button or filling a field
 			WebElement saveButton = wait
 					.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Save']")));
 			saveButton.click();
 			System.out.println("Profile Updated");
 			Thread.sleep(1000);
-			
-			//Move to notifications
+
+			// Move to notifications
 			Actions act = new Actions(driver);
 			act.moveToElement(driver.findElement(By.cssSelector(".nI-gNb-notif-center"))).build().perform();
 			Thread.sleep(500);
-			
+
 			// Log out
 			driver.findElement(By.cssSelector(".nI-gNb-drawer__icon")).click();
 			Thread.sleep(2000);
-			WebElement logout = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Logout"))));
+			WebElement logout = wait
+					.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Logout"))));
 			logout.click();
 			System.out.println("Profile Logout");
 
 		} catch (Exception e) {
 			System.out.println("Exception Type: " + e.getClass().getName());
-            System.out.println("Exception Message: " + e.getMessage());
+			System.out.println("Exception Message: " + e.getMessage());
 		} finally {
 			driver.quit();
 		}
